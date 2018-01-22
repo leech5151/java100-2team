f8a98821c80b219e6154a71d292baf8291e624cf
jung <angelpjn@naver.com>
2018-01-22 17:55:03 +0900

    sdaf

--- web/src/main/java/java100/app/web/ScoreController.java
+++ web/src/main/java/java100/app/web/ScoreController.java
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java100.app.dao.ScoreDao;
 import java100.app.domain.Score;
 import java100.app.service.ScoreService;
 
-@Controller
+@Controller        
 @RequestMapping("/score")
 public class ScoreController {
     
@@ -30,7 +29,7 @@ public class ScoreController {
 
         if (pageNo < 1) {
             pageNo = 1;
-        }
+        }   
         if (pageSize < 5 || pageSize > 15) {
             pageSize = 5;
         }

c0cfabad000538951b559e1b4af260168962ce12
jung <angelpjn@naver.com>
2018-01-25 20:22:07 +0900

    .

--- web/src/main/java/java100/app/dao/PetDao.java
+++ web/src/main/java/java100/app/dao/PetDao.java
@@ -1,12 +1,12 @@
 package java100.app.dao;
 
 import java.util.List;
-import java.util.Map;
 
 import java100.app.domain.Pet;
 
 public interface PetDao {
-    List<Pet> findAll(Map<String,Object> params);
+    
+    List<Pet> findAll(int searchNo);
     int countAll();
     int insert(Pet pet);
     int delete(int no);

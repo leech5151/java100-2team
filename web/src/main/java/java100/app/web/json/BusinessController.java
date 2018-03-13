c6687188410a884a2351f68e2e7343751959ac14
jung <angelpjn@naver.com>
2018-03-13 14:52:57 +0900

    ..

--- web/src/main/java/java100/app/web/json/BusinessController.java
+++ web/src/main/java/java100/app/web/json/BusinessController.java
@@ -77,14 +77,10 @@ public class BusinessController {
             @RequestParam(value="ps", defaultValue="12") int pageSize,
             @RequestParam(value="words", required=false) String[] words,
             @RequestParam(value="oc", required=false) String orderColumn,
-            @RequestParam(value="al", required=false) String align,
-            String category
-            ) throws Exception {
+            @RequestParam(value="al", required=false) String align) throws Exception {
 
         // UI 제어와 관련된 코드는 이렇게 페이지 컨트롤러에 두어야 한다.
         //
-        
-        System.out.println(category);
         
         if (pageNo < 1) {
             pageNo = 1;
@@ -93,7 +89,7 @@ public class BusinessController {
         if (pageSize < 12 || pageSize >36) {
             pageSize = 12;
         }
-        
+
         HashMap<String,Object> options = new HashMap<>();
         if (words != null && words[0].length() > 0) {
             options.put("words", words);
@@ -111,6 +107,7 @@ public class BusinessController {
         result.put("pageNo", pageNo);
         result.put("lastPageNo", lastPageNo);
         result.put("list", businessService.list(pageNo, pageSize, options));
+        
         return result;
     }
     
